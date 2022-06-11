9a29c0421dcaa8fa244d13775a60cd38ead007f1
Barza Nisar <barzanisar93@gmail.com>
2022-06-11 18:58:29 -0400

    launch fair multi node

--- scripts/multinode-wrapper.py
+++ scripts/multinode-wrapper.py
@@ -8,7 +8,7 @@ import sys, os
 
 num_nodes = int(os.environ['SLURM_NNODES'])
 node_id = int(os.environ['SLURM_NODEID'])
-node0 = 'learnfair' + os.environ['SLURM_NODELIST'][10:14]
-cmd = 'python {script} {cfg} --dist-url tcp://{node0}:1234 --dist-backend nccl --multiprocessing-distributed --world-size {ws} --rank {rank}'.format(
+node0 = 'gra' + os.environ['SLURM_NODELIST'][10:14]
+cmd = 'python {script} --cfg {cfg} --dist-url tcp://{node0}:1234 --dist-backend nccl --multiprocessing-distributed --world-size {ws} --rank {rank} --launcher fair'.format(
        script=sys.argv[1], cfg=sys.argv[2], node0=node0, ws=num_nodes, rank=node_id)
 os.system(cmd)
