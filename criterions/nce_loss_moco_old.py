87183bc3e6024d74da3d70fe0bb879935aa44442
Barza Nisar <barzanisar93@gmail.com>
2022-08-05 11:09:54 -0400

    Temp add old files

---
+++ criterions/nce_loss_moco_old.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+import logging
+import math
+import pprint
+
+import numpy as np
+import torch
+from torch import nn
+
+# utils
+@torch.no_grad()
+def concat_all_gather(tensor):
+    """
+    torch.dist.all_gather => Gathers key embeddings = normalized output 2 from all gpus in a list
+    Performs all_gather operation on the provided tensors.
+    *** Warning ***: torch.distributed.all_gather has no gradient.
+    """
+    if not (torch.distributed.is_initialized()):
+        return tensor
+    
+    tensors_gather = [torch.ones_like(tensor)
+                      for _ in range(torch.distributed.get_world_size())]
+    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+
+    output = torch.cat(tensors_gather, dim=0) # stack key embeddings vertically hcat[(batch size for gpu 1 x 128), (b2 x 128), (b3 x 128), (b4 x 128)] ==> [(row dim = b1+b2+b3+b4 , col dim = 128)]
+    # print("concat_all_gather each gpu's normalized output 2 shape")
+    # for each_tensor in tensors_gather:
+    #     print(each_tensor.shape)
+    # print("concat_all_gather output shape ", output.shape)
+    return output
+
+class NCELossMoco(nn.Module):
+    """
+    Distributed version of the NCE loss. It performs an "all_gather" to gather
+    the allocated buffers like memory no a single gpu. For this, Pytorch distributed
+    backend is used. If using NCCL, one must ensure that all the buffer are on GPU.
+    This class supports training using both NCE and CrossEntropy (InfoNCE).
+    """
+
+    def __init__(self, config):
+        super(NCELossMoco, self).__init__()
+
+        assert config["NCE_LOSS"]["LOSS_TYPE"] in [
+            "cross_entropy",
+        ], f"Supported types are cross_entropy."
+
+        self.loss_type = config["NCE_LOSS"]["LOSS_TYPE"]
+        self.loss_list = config["LOSS_TYPE"].split(",")
+        self.other_queue = config["OTHER_INPUT"]
+
+        self.npid0_w = float(config["within_format_weight0"])
+        self.npid1_w = float(config["within_format_weight1"])
+        self.cmc0_w = float(config["across_format_weight0"])
+        self.cmc1_w = float(config["across_format_weight1"])
+        
+        self.K = int(config["NCE_LOSS"]["NUM_NEGATIVES"])
+        self.dim = int(config["NCE_LOSS"]["EMBEDDING_DIM"])
+        self.T = float(config["NCE_LOSS"]["TEMPERATURE"])
+
+        self.register_buffer("queue", torch.randn(self.dim, self.K)) # queue of pointnet based negative key embeddings
+        self.queue = nn.functional.normalize(self.queue, dim=0)
+        self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
+        
+        if self.other_queue:
+            self.register_buffer("queue_other", torch.randn(self.dim, self.K))  #queue of UNET based negative key embeddings
+            self.queue_other = nn.functional.normalize(self.queue_other, dim=0)
+            self.register_buffer("queue_other_ptr", torch.zeros(1, dtype=torch.long))
+            
+        # cross-entropy loss. Also called InfoNCE
+        self.xe_criterion = nn.CrossEntropyLoss()
+
+        # other constants
+        self.normalize_embedding = config["NCE_LOSS"]["NORM_EMBEDDING"]
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(config)
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self, keys, okeys=None):
+        # gather keys before updating queue
+        keys = concat_all_gather(keys)
+        
+        batch_size = keys.shape[0]
+        
+        ptr = int(self.queue_ptr)
+        #assert self.K % batch_size == 0  # for simplicity
+        if (ptr + batch_size) > self.queue.shape[1]:
+            batch_size = self.queue.shape[1] - ptr
+
+        # replace the keys at ptr (dequeue and enqueue)
+        self.queue[:, ptr:ptr + batch_size] = torch.transpose(keys, 0, 1)[:, 0:batch_size] #we could also pick randomly batch size num voxels
+        ptr = (ptr + batch_size) % self.K  # move pointer
+        
+        self.queue_ptr[0] = ptr
+
+        if self.other_queue:
+            # gather keys before updating queue
+            okeys = concat_all_gather(okeys)
+                
+            other_ptr = int(self.queue_other_ptr)
+        
+            # replace the keys at ptr (dequeue and enqueue)
+            self.queue_other[:, other_ptr:other_ptr + batch_size] = torch.transpose(okeys, 0, 1)#okeys.T
+            other_ptr = (other_ptr + batch_size) % self.K  # move pointer
+        
+            self.queue_other_ptr[0] = other_ptr
+                                                                        
+    def forward(self, output):
+        # assert isinstance(
+        #     output, list
+        # ), "Model output should be a list of tensors. Got Type {}".format(type(output))
+        
+        if self.normalize_embedding:
+            normalized_output1 = nn.functional.normalize(output[0], dim=1, p=2) #query pointnet embedding
+            normalized_output2 = nn.functional.normalize(output[1], dim=1, p=2) #key pointnet embedding
+            if self.other_queue:
+                normalized_output3 = nn.functional.normalize(output[2], dim=1, p=2) #query unet embedding
+                normalized_output4 = nn.functional.normalize(output[3], dim=1, p=2) #key unet embedding
+
+        # Voxelized Depth Contrast
+        if output[0]['vdc_voxel_bzyx'] is not None:
+            coords_0 = output[0]['vdc_voxel_bzyx'] # vox_coords from query encoder = list of len total num voxels=1181, each element has a numpy array with a voxel coord = bzyx
+            coords_1 = output[1]['vdc_voxel_bzyx'] # vox_coords from key encoder
+
+            # Find matching vox_coords in both in order to rearrange normalized output1 and output2 in the order of matching voxel features
+            map_coord0_idx0 = {str(coords_0[i, :]): i for i in range(coords_0.shape[0])}
+            matched_idx_0 = []
+            matched_idx_1 = []
+            for idx_1 in range(coords_1.shape[0]):
+                if str(coords_1[idx_1, :]) in map_coord0_idx0:
+                    idx_0 = map_coord0_idx0[str(coords_1[idx_1, :])]
+                    matched_idx_0.append(idx_0)
+                    matched_idx_1.append(idx_1)
+
+            normalized_output1 = normalized_output1[matched_idx_0, :] #(matched num voxels, 128)
+            normalized_output2 = normalized_output2[matched_idx_1, :] #(matched num voxels, 128)
+
+        # positive logits: Nx1 = batch size of positive examples (or matched num voxels)x 1
+        l_pos = torch.einsum('nc,nc->n', [normalized_output1, normalized_output2]).unsqueeze(-1) #(v_i_1).transpose() * v_i_2 => dim (n = matched num voxels)
+        
+        # negative logits: NxK
+        l_neg = torch.einsum('nc,ck->nk', [normalized_output1, self.queue.clone().detach()])
+        
+        # logits: Nx(1+K)
+        logits = torch.cat([l_pos, l_neg], dim=1)
+        
+        # apply temperature
+        logits /= self.T
+
+        if self.other_queue:
+            
+            l_pos_p2i = torch.einsum('nc,nc->n', [normalized_output1, normalized_output4]).unsqueeze(-1)
+            l_neg_p2i = torch.einsum('nc,ck->nk', [normalized_output1, self.queue_other.clone().detach()])
+            logits_p2i = torch.cat([l_pos_p2i, l_neg_p2i], dim=1)
+            logits_p2i /= self.T
+
+            
+            l_pos_i2p = torch.einsum('nc,nc->n', [normalized_output3, normalized_output2]).unsqueeze(-1)
+            l_neg_i2p = torch.einsum('nc,ck->nk', [normalized_output3, self.queue.clone().detach()])
+            logits_i2p = torch.cat([l_pos_i2p, l_neg_i2p], dim=1)
+            logits_i2p /= self.T
+
+            
+            l_pos_other = torch.einsum('nc,nc->n', [normalized_output3, normalized_output4]).unsqueeze(-1)
+            l_neg_other = torch.einsum('nc,ck->nk', [normalized_output3, self.queue_other.clone().detach()])
+            logits_other = torch.cat([l_pos_other, l_neg_other], dim=1)
+            logits_other /= (self.T)
+            
+        if self.other_queue:
+            self._dequeue_and_enqueue(normalized_output2, okeys=normalized_output4)
+        else:
+            self._dequeue_and_enqueue(normalized_output2)
+
+        
+        labels = torch.zeros(
+            logits.shape[0], device=logits.device, dtype=torch.int64
+        ) # because zero'th class is the true class
+        
+        loss_npid = self.xe_criterion(torch.squeeze(logits), labels) #loss between pointnet query and key embedding
+
+        loss_npid_other = torch.tensor(0)
+        loss_cmc_p2i = torch.tensor(0)
+        loss_cmc_i2p = torch.tensor(0)
+        
+        if self.other_queue:
+            loss_cmc_p2i = self.xe_criterion(torch.squeeze(logits_p2i), labels) #loss between pointnet query and unet key embedding
+            loss_cmc_i2p = self.xe_criterion(torch.squeeze(logits_i2p), labels) #loss between unet query and pointnet key embedding
+            loss_npid_other = self.xe_criterion(torch.squeeze(logits_other), labels) #loss between unet query and key embedding
+            
+            curr_loss = 0
+            for ltype in self.loss_list:
+                if ltype == "CMC":
+                    curr_loss += loss_cmc_p2i * self.cmc0_w + loss_cmc_i2p * self.cmc1_w
+                elif ltype == "NPID":
+                    curr_loss += loss_npid * self.npid0_w
+                    curr_loss += loss_npid_other * self.npid1_w
+        else:
+            curr_loss = 0
+            curr_loss += loss_npid * self.npid0_w
+                        
+        loss = curr_loss
+
+        return loss, [loss_npid, loss_npid_other, loss_cmc_p2i, loss_cmc_i2p]
+
+    def __repr__(self):
+        repr_dict = {
+            "name": self._get_name(),
+            "loss_type": self.loss_type,
+        }
+        return pprint.pformat(repr_dict, indent=2)
---
+++ models/trunks/pointnet2_backbone_old.py
@@ -0,0 +1,316 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import os
+import sys
+import numpy as np
+
+from models.trunks.mlp import MLP
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) #trunks
+ROOT_DIR = os.path.dirname(ROOT_DIR) #models
+ROOT_DIR = os.path.dirname(ROOT_DIR) #DepthContrast
+sys.path.append(os.path.join(ROOT_DIR, 'third_party', 'OpenPCDet', "pcdet"))
+
+from ops.pointnet2.pointnet2_batch import pointnet2_modules
+try:
+    try:
+        try:
+            from spconv.utils import VoxelGeneratorV2 as VoxelGenerator
+            SPCONV_VER = 1
+        except:
+            from spconv.utils import VoxelGenerator
+            SPCONV_VER = 1
+    except:
+        #from spconv.utils import Point2VoxelCPU3d as VoxelGenerator
+        #from spconv.utils import Point2VoxelGPU3d as VoxelGenerator
+        from spconv.pytorch.utils import PointToVoxel as VoxelGenerator
+        SPCONV_VER = 2
+except:
+    pass
+
+def point_to_voxel_func(device = torch.device("cpu:0")):
+    VOXEL_SIZE = [5.0, 5.0, 6.0]
+    ### Waymo lidar range
+    #POINT_RANGE = np.array([  0. , -75. ,  -3. ,  75.0,  75. ,   3. ], dtype=np.float32)
+    POINT_RANGE = np.array([0, -40, -3, 70.4, 40, 1], dtype=np.float32) ### KITTI and DENSE
+    MAX_POINTS_PER_VOXEL = 2000
+    MAX_NUMBER_OF_VOXELS = 400
+    NUM_POINT_FEATURES= 3+128
+    if SPCONV_VER == 1:
+        voxel_generator = VoxelGenerator(
+            voxel_size=VOXEL_SIZE,
+            point_cloud_range=POINT_RANGE,
+            max_num_points=MAX_POINTS_PER_VOXEL,
+            max_voxels=MAX_NUMBER_OF_VOXELS,
+            device = device
+        )
+    else:
+        voxel_generator = VoxelGenerator(
+            vsize_xyz=VOXEL_SIZE,
+            coors_range_xyz=POINT_RANGE,
+            num_point_features=NUM_POINT_FEATURES,
+            max_num_points_per_voxel=MAX_POINTS_PER_VOXEL,
+            max_num_voxels=MAX_NUMBER_OF_VOXELS,
+            device = device
+        )
+
+    return voxel_generator
+
+class PointNet2MSG(nn.Module):
+    def __init__(self, use_mlp=False, mlp_dim=None):
+        super().__init__()
+
+        input_channels = 4
+        
+        self.SA_modules = nn.ModuleList()
+        channel_in = input_channels - 3 #  C
+
+        self.num_points_each_layer = []
+        skip_channel_list = [input_channels - 3]
+        SA_CONFIG = {'NPOINTS': [4096, 1024, 256, 64],
+                     'RADIUS': [[0.1, 0.5], [0.5, 1.0], [1.0, 2.0], [2.0, 4.0]],
+                     'NSAMPLE': [[16, 32], [16, 32], [16, 32], [16, 32]],
+                     'MLPS': [[[16, 16, 32], [32, 32, 64]], [[64, 64, 128], [64, 96, 128]], [[128, 196, 256], [128, 196, 256]], [[256, 256, 512], [256, 384, 512]]]}
+        # (MSG) Multiscale SA(K=4096, r=[0.1, 0.5], PointNets (for r = 0.1 and r = 0.5 respectively) = [[16, 16, 32], [32, 32, 64]])
+
+        FP_MLPS = [[128, 128], [256, 256], [512, 512], [512, 512]] #[last FP= [1+256, 128, 128], second last fp = [96+512, 256, 256], fp = [256+512, 512, 512], fp=[512 + 1024, 512, 512]]
+        # The correct order of FP1 = [Cin = Cout of SA second last level + Cout of SA last = 512 + 1024, 512, 512]
+        # -> FP2 = [Cin = Cout of SA 3rd last level + Cout of FP1 = 256+512, 512, 512]
+        # -> FP3 = [Cin = Cout of SA 4th last level + Cout of FP2 = 96+512, 256, 256]
+        # -> FP4 = [Cin = Cout of SA 1st level + Cout of FP3 = 1+256, 128, 128]
+        for k in range(SA_CONFIG["NPOINTS"].__len__()):
+            mlps = SA_CONFIG["MLPS"][k].copy() #SA1 = sample 4096 centroids, group points that fall within radius of 0.1 for each centroid, group for r= 0.5, mlps = [for r=0.1->[16, 16, 32], for r=0.5 -> [32, 32, 64]]
+            channel_out = 0
+            for idx in range(mlps.__len__()):
+                mlps[idx] = [channel_in] + mlps[idx]
+                channel_out += mlps[idx][-1]
+
+            self.SA_modules.append(
+                pointnet2_modules.PointnetSAModuleMSG(
+                    npoint=SA_CONFIG["NPOINTS"][k],
+                    radii=SA_CONFIG["RADIUS"][k],
+                    nsamples=SA_CONFIG["NSAMPLE"][k],
+                    mlps=mlps,
+                    use_xyz=True,
+                )
+            )
+            # mlps for SA1 becomes [Cin = xyz+(Cout of previous layer or 1 (intensity)) = 3+1 = 4, 16, 16, 32], [3+1, 32, 32, 64]
+            # Cout of SA1 = 32 (feature vector for r = 0.1) + 64 (feature vector for r = 0.5) = 96
+
+            #mlps for SA2 becomes [Cin=xyz+96 = 99, 64,64,128], [64, 96, 128], Cout = 128+128 = 256
+            skip_channel_list.append(channel_out)
+            channel_in = channel_out
+
+        self.FP_modules = nn.ModuleList()
+
+        for k in range(FP_MLPS.__len__()):
+            pre_channel = FP_MLPS[k + 1][-1] if k + 1 < len(FP_MLPS) else channel_out
+            self.FP_modules.append(
+                pointnet2_modules.PointnetFPModule(
+                    mlp=[pre_channel + skip_channel_list[k]] + FP_MLPS[k]
+                )
+            )
+
+        self.num_point_features = FP_MLPS[0][-1] # 128
+        
+        self.all_feat_names = [
+            "fp2",
+        ]
+
+        if use_mlp:
+            self.use_mlp = True
+            self.head = MLP(mlp_dim) # projection head
+
+            
+    def break_up_pc(self, pc):
+        #batch_idx = pc[:, 0]
+        xyz = pc[:, :, 0:3].contiguous()
+        features = (pc[:, :, 3:].contiguous() if pc.size(-1) > 3 else None)
+        return xyz, features
+
+    def forward(self, pointcloud: torch.cuda.FloatTensor, out_feat_keys=None, aug_matrix=None):
+        raise NotImplementedError
+        
+
+class PointNet2MSG_VoxelizedDepthContrast(PointNet2MSG):
+    def __init__(self, use_mlp=False, mlp_dim=None, linear_probe = False):
+        super().__init__(use_mlp, mlp_dim)
+        self.linear_probe = linear_probe
+
+    def forward(self, pointcloud: torch.cuda.FloatTensor, out_feat_keys=None, aug_matrix=None):
+        """
+        Args:
+            batch_dict:
+                batch_size: int
+                vfe_features: (num_voxels, C)
+                points: (num_points, 4 + C), [batch_idx, x, y, z, ...]
+        Returns:
+            batch_dict:
+                encoded_spconv_tensor: sparse tensor
+                point_features: (N, C)
+        """
+        batch_size = pointcloud.shape[0]
+        points = pointcloud
+        xyz, features = self.break_up_pc(points) # xyz = (8, 16384, 3), features (intensity) = (8, 1, 16384)
+
+        features = features.view(batch_size, -1, features.shape[-1]).permute(0, 2, 1).contiguous() if features is not None else None
+        l_xyz, l_features = [xyz], [features]
+        for i in range(len(self.SA_modules)):
+            assert l_features[i].is_contiguous()
+            li_xyz, li_features = self.SA_modules[i](l_xyz[i], l_features[i])
+            assert li_features.is_contiguous()
+            l_xyz.append(li_xyz)
+            l_features.append(li_features)
+
+        for i in range(-1, -(len(self.FP_modules) + 1), -1):
+            assert l_features[i - 1].is_contiguous()
+            assert l_features[i].is_contiguous()
+            l_features[i - 1] = self.FP_modules[i](
+                l_xyz[i - 1], l_xyz[i], l_features[i - 1], l_features[i]
+            )  # (B, C, N)
+            assert l_features[i - 1].is_contiguous()
+
+        # print("Transformed" , l_xyz[0][0,0,:])
+        # print("Transformed" , xyz[0,0,:])
+        assert np.abs((l_xyz[0] - xyz).detach().cpu().numpy()).max() < 1e-4
+
+        # Undo transformation for voxelizing and linear probe
+        xyz = xyz @ aug_matrix.inverse() # xyz = (8, 16384, 3)
+        # print("Transformed" , l_xyz[0][0,0,:])
+        # print("Un-Transformed" , xyz[0,0,:])
+        # print("Un-Transformed" , l_xyz[0][0,0,:] @ aug_matrix[0].inverse())
+
+        point_features = l_features[0] #(B=8, 128, num points = 16384)
+        end_points = {}
+        if not self.linear_probe:
+            #Voxelize pointcloud and avg/max pool point features in each voxel to get voxel wise features
+            xyz_point_features = torch.cat([xyz, point_features.permute(0, 2, 1).contiguous()], 2) # (8, 16394, 131=3+128)
+            batch_voxel_features_list = []
+            vox_coords = []
+            
+            voxel_generator = point_to_voxel_func(device=xyz_point_features.device)
+            for pc_idx in range(batch_size):
+                voxel_features, coordinates, voxel_num_points = voxel_generator(xyz_point_features[pc_idx]) # voxel_features = (num voxels, max points per voxel, 3+features_dim) #coords = z_grid_idx, y_idx, x_idx 
+                vox_coords.append(np.pad(coordinates.cpu(), ((0, 0), (1, 0)), mode='constant', constant_values=pc_idx))
+                # Mean VFE: find mean of point features in each voxel # try max as well
+                points_mean = voxel_features[:, :, :].sum(dim=1, keepdim=False)
+                normalizer = torch.clamp_min(voxel_num_points.view(-1, 1), min=1.0).type_as(voxel_features)
+                points_mean = points_mean / normalizer
+                voxel_features = points_mean.contiguous()
+                batch_voxel_features_list.append(voxel_features[:, 3:]) #only append features, not mean x,y,z, points
+
+                #Or max pool point features inside each voxel to get vfe
+                #voxel_features = F.max_pool1d(voxel_features.permute(0, 2, 1).contiguous(), voxel_features.shape[1])
+                #batch_voxel_features_list.append(voxel_features)
+
+            batch_voxel_feats = torch.cat(batch_voxel_features_list, 0) #(total num voxels=1181, 128) = ( 8 * numvoxels in each pc, 128)
+            vox_coords = np.concatenate(vox_coords, axis=0) #(total num vox = 1181, 4)  = [batch_idx, z_idx,y_idx,x_idx]
+            
+            end_points['fp2_features'] = batch_voxel_feats 
+        else:
+            end_points['fp2_features'] = point_features
+    
+        out_feats = [None] * len(out_feat_keys)
+        for key in out_feat_keys:
+            feat = end_points[key+"_features"]
+
+            if self.linear_probe:
+                feat = feat.view(batch_size, -1, feat.shape[-1]).permute(0, 2, 1).contiguous() #(8, 128, npoints=16384) -> (8, 16384, 128) 
+
+            if self.use_mlp:
+                feat = self.head(feat)
+            out_feats[out_feat_keys.index(key)] = feat
+        
+        point_coords = None
+        if self.linear_probe:
+            point_coords = xyz
+            vox_coords = None
+        
+        # out_feats[0] = (1181, 128), vox_coords = (1181, 4=bzyx), point_coords = (8, 16384, 3=xyz)
+        return out_feats, vox_coords, point_coords
+
+
+
+class PointNet2MSG_DepthContrast(PointNet2MSG):
+    def __init__(self, use_mlp=False, mlp_dim=None, linear_probe = False):
+        super().__init__(use_mlp, mlp_dim)
+        self.linear_probe = linear_probe
+
+    def forward(self, pointcloud: torch.cuda.FloatTensor, out_feat_keys=None, aug_matrix=None):
+        """
+        Args:
+            batch_dict:
+                batch_size: int
+                vfe_features: (num_voxels, C)
+                points: (num_points, 4 + C), [batch_idx, x, y, z, ...]
+        Returns:
+            batch_dict:
+                encoded_spconv_tensor: sparse tensor
+                point_features: (N, C)
+        """
+        batch_size = pointcloud.shape[0]
+        points = pointcloud #(8, 16384, 4)
+        xyz, features = self.break_up_pc(points) #(8, 16384, 3), #(8, 16384, 1)
+
+        features = features.view(batch_size, -1, features.shape[-1]).permute(0, 2, 1).contiguous() if features is not None else None # (8, 16384, 1) -> (8, 1, 16384)
+        l_xyz, l_features = [xyz], [features]
+        for i in range(len(self.SA_modules)):
+            assert l_features[i].is_contiguous()
+            li_xyz, li_features = self.SA_modules[i](l_xyz[i], l_features[i])
+            assert li_features.is_contiguous()
+            l_xyz.append(li_xyz)
+            l_features.append(li_features)
+
+        for i in range(-1, -(len(self.FP_modules) + 1), -1):
+            assert l_features[i - 1].is_contiguous()
+            assert l_features[i].is_contiguous()
+            l_features[i - 1] = self.FP_modules[i](
+                l_xyz[i - 1], l_xyz[i], l_features[i - 1], l_features[i]
+            )  # (B, C, N)
+            assert l_features[i - 1].is_contiguous()
+
+        # print("Transformed" , l_xyz[0][0,0,:])
+        # print("Transformed" , xyz[0,0,:])
+        # assert np.abs((l_xyz[0] - xyz).detach().cpu().numpy()).max() < 1e-4
+
+        # Undo transformation for linear probe
+        # xyz = xyz @ aug_matrix.inverse()
+        # print("Transformed" , l_xyz[0][0,0,:])
+        # print("Un-Transformed" , xyz[0,0,:])
+        # print("Un-Transformed" , l_xyz[0][0,0,:] @ aug_matrix[0].inverse())
+
+        point_features = l_features[0] #(B=8, 128, num points = 16384)
+        
+        end_points = {}
+        end_points['fp2_features'] = point_features
+    
+        out_feats = [None] * len(out_feat_keys)
+        for key in out_feat_keys:
+            feat = end_points[key+"_features"]
+            if not self.linear_probe:
+                nump = feat.shape[-1] # num points original
+                # get one feature vector of dim 128 for the entire point cloud
+                feat = torch.squeeze(F.max_pool1d(feat, nump))
+            else:
+                feat = feat.view(batch_size, -1, feat.shape[-1]).permute(0, 2, 1).contiguous() #(8, 128, npoints=16384) -> (8, 16384, 128) 
+
+            if self.use_mlp:
+                feat = self.head(feat) #linear probe out (8=batch, 16384=npoints, 5=nclasses), #Dc out (8, 128)
+            out_feats[out_feat_keys.index(key)] = feat
+        
+        vox_coords = None
+        point_coords = None
+        if self.linear_probe:
+            # Undo transformation for linear probe
+            point_coords = xyz @ aug_matrix.inverse()
+            
+        return out_feats, vox_coords, point_coords
+
