7196b0076dafb26d38d9c3371d9873f02b006f1f
Barza Nisar <barzanisar93@gmail.com>
2022-08-05 11:04:04 -0400

    minor

--- make_sbatch.py
+++ make_sbatch.py
@@ -2,15 +2,15 @@ import glob
 from pathlib import Path 
 
 root_path = '/home/barza/DepthContrast'
-cfg_dir = 'pointnet_train_all_FOV3000_60'
-cc_infos_dir = 'DepthContrast_FOV3000_Infos'
+cfg_dir = 'pointnet_train_all_60'
+cc_infos_dir = 'DepthContrast_Infos'
 cfg_dir_path = f'{root_path}/configs/{cfg_dir}'
 models = ['dc', 'vdc']
-tcp_port = 18800
+tcp_port = 18825
 sbatch_file = f'{root_path}/run_sbatch.sh'
 
-with open(sbatch_file, 'w') as f:
-    f.write('#FOV3000\n')
+with open(sbatch_file, 'a') as f:
+    f.write('#360deg\n')
 
 for model in models:
     cfg_files = glob.glob(f'{cfg_dir_path}/{model}/*.yaml')
