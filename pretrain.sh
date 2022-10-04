783ce4848a87ca6545269f4c1bd8ac5bc6556c9e
Barza Nisar <barzanisar93@gmail.com>
2022-10-03 23:13:16 -0400

    reduce batch size to avoid cuda mem error

--- pretrain.sh
+++ pretrain.sh
@@ -1,21 +1,22 @@
 # FOV3000
 
-#Dense-Kitti-SemKitti-Pretrain
+# #Dense-Kitti-SemKitti-Pretrain
 
-#SegContrast
-sbatch --time=01:00:00 --array=1-1%1 --job-name=seg_1in2_cube_up_dense_semkitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_semkitti.sh --cluster --tcp_port 18949 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_1in2_cube_up_dense_semkitti.yaml
-sbatch --time=01:00:00 --array=1-1%1 --job-name=seg_dense_semkitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_semkitti.sh --cluster --tcp_port 18640 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_dense_semkitti.yaml
+# #SegContrast
+# sbatch --time=01:00:00 --array=1-1%1 --job-name=seg_1in2_cube_up_dense_semkitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_semkitti.sh --cluster --tcp_port 18949 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_1in2_cube_up_dense_semkitti.yaml
+# sbatch --time=01:00:00 --array=1-1%1 --job-name=seg_dense_semkitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_semkitti.sh --cluster --tcp_port 18640 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_dense_semkitti.yaml
 
-#DepthContrast
+# #DepthContrast
 sbatch --time=01:00:00 --array=1-1%1 --job-name=dc_1in2_cube_up_dense_semkitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_semkitti.sh --tcp_port 19949 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in2_cube_up_dense_semkitti.yaml
-sbatch --time=01:00:00 --array=1-1%1 --job-name=dc_dense_semkitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_semkitti.sh --tcp_port 19640 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_dense_semkitti.yaml
+# sbatch --time=01:00:00 --array=1-1%1 --job-name=dc_dense_semkitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_semkitti.sh --tcp_port 19640 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_dense_semkitti.yaml
 
-#Dense-Kitti-Pretrain
+# #Dense-Kitti-Pretrain
 
-#SegContrast
-sbatch --time=01:00:00 --array=1-1%1 --job-name=seg_1in2_cube_up_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --cluster --tcp_port 18969 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_1in2_cube_up_dense_kitti.yaml
-sbatch --time=01:00:00 --array=1-1%1 --job-name=seg_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --cluster --tcp_port 18660 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_dense_kitti.yaml
+# #SegContrast
+# sbatch --time=01:00:00 --array=1-1%1 --job-name=seg_1in2_cube_up_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --cluster --tcp_port 18969 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_1in2_cube_up_dense_kitti.yaml
+# sbatch --time=01:00:00 --array=1-1%1 --job-name=seg_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --cluster --tcp_port 18660 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_dense_kitti.yaml
 
+# Done pretraining upto 330 epochs
 # #DepthContrast
 # sbatch --time=24:00:00 --array=1-2%1 --job-name=dc_1in2_cube_up_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in2_cube_up_dense_kitti.yaml
 # sbatch --time=24:00:00 --array=1-2%1 --job-name=dc_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19660 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_dense_kitti.yaml
