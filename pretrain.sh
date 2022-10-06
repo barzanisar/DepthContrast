54cb58fdfcd69e957c34d310ea33013c22f1877d
Barza Nisar <barzanisar93@gmail.com>
2022-10-06 16:50:11 -0400

    weather ablations

--- pretrain.sh
+++ pretrain.sh
@@ -13,14 +13,34 @@
 #Dense-Kitti-Pretrain
 
 #SegContrast
-sbatch --time=15:00:00 --gres=gpu:t4:2 --array=1-3%1 --job-name=seg_1in2_cube_up_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --cluster --tcp_port 18969 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_1in2_cube_up_dense_kitti.yaml
-sbatch --time=15:00:00 --gres=gpu:t4:2 --array=1-3%1 --job-name=seg_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --cluster --tcp_port 18660 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_dense_kitti.yaml
+#sbatch --time=03:00:00 --gres=gpu:t4:4 --array=1-10%1 --job-name=seg_1in2_cube_up_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --cluster --tcp_port 18969 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_1in2_cube_up_dense_kitti.yaml
+#sbatch --time=03:00:00 --gres=gpu:t4:4 --array=1-10%1 --job-name=seg_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --cluster --tcp_port 18660 --cfg_file configs/pointnet_train_all_FOV3000_60/seg/seg_dense_kitti.yaml
+
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=seg_1in2_upF_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/seg_1in2_upF_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=seg_1in10_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/seg_1in10_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=seg_1in10_upF_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/seg_1in10_upF_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=seg_1in2_snow_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/seg_1in2_snow_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=seg_1in2_fog_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/seg_1in2_fog_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=seg_1in2_snow_fog_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/seg_1in2_snow_fog_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=seg_1in2_up_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/seg_1in2_up_dense_kitti.yaml
 
 # Done pretraining upto 330 epochs
 # #DepthContrast
 # sbatch --time=24:00:00 --array=1-2%1 --job-name=dc_1in2_cube_up_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in2_cube_up_dense_kitti.yaml
 # sbatch --time=24:00:00 --array=1-2%1 --job-name=dc_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19660 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_dense_kitti.yaml
 
+#Weather Ablations
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=dc_1in2_upF_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in2_upF_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=dc_1in10_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in10_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=dc_1in10_upF_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in10_upF_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=dc_1in2_snow_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in2_snow_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=dc_1in2_fog_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in2_fog_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=dc_1in2_snow_fog_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in2_snow_fog_dense_kitti.yaml
+sbatch --time=04:00:00 --gres=gpu:t4:4 --array=1-6%1 --job-name=dc_1in2_up_dense_kitti --mail-user=barzanisar93@gmail.com scripts/compute_canada_train_depth_contrast_dense_kitti.sh --tcp_port 19969 --cfg_file configs/pointnet_train_all_FOV3000_60/dc/dc_1in2_up_dense_kitti.yaml
+
+
+
+#____________________________REMOVE_____________________________________________________________________________________________
 #TODO:
 # Add ablation: seg-snow/fog sim, seg-adversetoclear
 
