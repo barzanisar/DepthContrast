9efd3ce2bceabca69f498b6d2444c9e527b77353
Barza Nisar <barzanisar93@gmail.com>
2023-07-19 02:12:49 -0400

    multinode try

--- scripts/launch_ddp.sh
+++ scripts/launch_ddp.sh
@@ -8,6 +8,29 @@
 # echo "command line args for launch_ddp.sh"
 # echo "$1 $2 $3 $4 $5"
 
+# Extract Dataset
+echo "Extracting Waymo data"
+TMP_DATA_DIR=$SLURM_TMPDIR/data
+
+# echo "Unzipping $DATA_DIR/waymo_processed_data_10.zip to $TMP_DATA_DIR"
+# unzip -qq $DATA_DIR/waymo_processed_data_10.zip -d $TMP_DATA_DIR
+
+# echo "Unzipping $DATA_DIR/Infos/waymo_processed_data_10_infos.zip to $TMP_DATA_DIR"
+# unzip -qq $DATA_DIR/Infos/waymo_processed_data_10_infos.zip -d $TMP_DATA_DIR
+
+# echo "Unzipping $DATA_DIR/Infos/waymo_infos.zip to $TMP_DATA_DIR"
+# unzip -qq $DATA_DIR/Infos/waymo_infos.zip -d $TMP_DATA_DIR
+
+echo "Unzipping $DATA_DIR/waymo_processed_data_10_short.zip to $TMP_DATA_DIR"
+unzip -qq $DATA_DIR/waymo_processed_data_10_short.zip -d $TMP_DATA_DIR
+
+echo "Unzipping $DATA_DIR/waymo_processed_data_10_short_infos.zip to $TMP_DATA_DIR"
+unzip -qq $DATA_DIR/waymo_processed_data_10_short_infos.zip -d $TMP_DATA_DIR
+
+echo "Unzipping $DATA_DIR/waymo_processed_data_10_short_gt_database_train_sampled_1.zip to $TMP_DATA_DIR"
+unzip -qq $DATA_DIR/waymo_processed_data_10_short_gt_database_train_sampled_1.zip -d $TMP_DATA_DIR
+
+echo "Done extracting Waymo data"
 
 # Get last element in string and increment by 1
 NUM_GPUS="${CUDA_VISIBLE_DEVICES: -1}"
