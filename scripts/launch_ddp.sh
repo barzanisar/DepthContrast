a79559a4d59984bd71f6bbf46c6c6393cbc8ff7b
Barza Nisar <barzanisar93@gmail.com>
2023-10-04 16:18:02 -0400

    minor

--- scripts/launch_ddp.sh
+++ scripts/launch_ddp.sh
@@ -12,8 +12,8 @@
 echo "Extracting Waymo data in Node: $SLURM_NODEID, SLURM_TMPDIR: $SLURM_TMPDIR"
 TMP_DATA_DIR=$SLURM_TMPDIR/data
 
-# echo "Unzipping $DATA_DIR/waymo_processed_data_10.zip to $TMP_DATA_DIR"
-# unzip -qq $DATA_DIR/waymo_processed_data_10.zip -d $TMP_DATA_DIR
+echo "Unzipping $DATA_DIR/waymo_processed_data_10.zip to $TMP_DATA_DIR"
+unzip -qq $DATA_DIR/waymo_processed_data_10.zip -d $TMP_DATA_DIR
 
 # echo "Unzipping $DATA_DIR/Infos/waymo_processed_data_10_infos.zip to $TMP_DATA_DIR"
 # unzip -qq $DATA_DIR/Infos/waymo_processed_data_10_infos.zip -d $TMP_DATA_DIR
@@ -21,8 +21,8 @@ TMP_DATA_DIR=$SLURM_TMPDIR/data
 # echo "Unzipping $DATA_DIR/Infos/waymo_infos.zip to $TMP_DATA_DIR"
 # unzip -qq $DATA_DIR/Infos/waymo_infos.zip -d $TMP_DATA_DIR
 
-echo "Unzipping $DATA_DIR/waymo_processed_data_10_short.zip to $TMP_DATA_DIR"
-unzip -qq $DATA_DIR/waymo_processed_data_10_short.zip -d $TMP_DATA_DIR
+# echo "Unzipping $DATA_DIR/waymo_processed_data_10_short.zip to $TMP_DATA_DIR"
+# unzip -qq $DATA_DIR/waymo_processed_data_10_short.zip -d $TMP_DATA_DIR
 
 echo "Copying $DATA_DIR/ImageSets to $TMP_DATA_DIR"
 cp -r $DATA_DIR/ImageSets -d $TMP_DATA_DIR
