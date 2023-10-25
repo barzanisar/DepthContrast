f90e548fea59c5818fe6faafb40c3df8581527a2
Barza Nisar <barzanisar93@gmail.com>
2023-10-24 23:25:31 -0400

    depth dataset cleaning

--- datasets/depth_dataset.py
+++ datasets/depth_dataset.py
@@ -50,20 +50,11 @@ class DepthContrastDataset(Dataset):
         self.pretraining = self.mode == 'train' and not self.linear_probe
         self.logger = logger
         self.cfg = cfg
-        #self.batchsize_per_replica = cfg["BATCHSIZE_PER_REPLICA"]
-        #self.dataset_names = cfg["DATASET_NAMES"] #TODO: is this needed?
         self.root_path = (Path(__file__) / '../..').resolve()  # DepthContrast
         self.point_cloud_range = np.array(cfg["POINT_CLOUD_RANGE"], dtype=np.float32)
         self.class_names = cfg["CLASS_NAMES"]
         self.used_num_point_features  = 4
 
-        # if "GT_SAMPLING" in self.cfg:
-        #     self.db_sampler = database_sampler.DataBaseSampler(
-        #     root_path=self.root_path / cfg["DATA_PATH"],
-        #     sampler_cfg=cfg["GT_SAMPLING"],
-        #     class_names=self.class_names,
-        #     logger=self.logger
-        # )
         self.data_augmentor = data_augmentor.DataAugmentor(self.cfg["POINT_TRANSFORMS"]) if self.pretraining else None
 
 
@@ -71,7 +62,7 @@ class DepthContrastDataset(Dataset):
         self.grid_size = None
         self.voxel_size = None
         self.depth_downsample_factor = None 
-        if ("Lidar" in cfg) and cfg["VOX"]:
+        if cfg["VOX"]:
             self.voxel_size = [0.1, 0.1, 0.15] #[0.05, 0.05, 0.1]
 
             self.MAX_POINTS_PER_VOXEL = 5
@@ -95,62 +86,34 @@ class DepthContrastDataset(Dataset):
             self.grid_size = np.round(grid_size).astype(np.int64)
 
     def toVox(self, points):
-        if "Lidar" in self.cfg:
-            if SPCONV_VER==1:
-                voxel_output = self.voxel_generator.generate(points)
-            else:
-                voxel_output = self.voxel_generator(torch.from_numpy(points).contiguous())
-            if isinstance(voxel_output, dict):
-                voxels, coordinates, num_points = \
-                                                  voxel_output['voxels'], voxel_output['coordinates'], voxel_output['num_points_per_voxel']
-            else:
-                voxels, coordinates, num_points = voxel_output
+        if SPCONV_VER==1:
+            voxel_output = self.voxel_generator.generate(points)
+        else:
+            voxel_output = self.voxel_generator(torch.from_numpy(points).contiguous())
+        if isinstance(voxel_output, dict):
+            voxels, coordinates, num_points = \
+                                                voxel_output['voxels'], voxel_output['coordinates'], voxel_output['num_points_per_voxel']
+        else:
+            voxels, coordinates, num_points = voxel_output
 
-            data_dict = {}
-            data_dict['voxels'] = voxels #dim =(num_voxels, max_points_per_voxel=5, coords+feats = 3+1=4), gives xyzi value for each point in each voxel, if num points in voxel < 5, fill in zeros
-            data_dict['voxel_coords'] = coordinates #dim= (num voxels, 3), gives z,y,x grid coord of each voxel 
-            data_dict['voxel_num_points'] = num_points # dim = num_voxels, gives num points in each voxel
-            return data_dict
+        data_dict = {}
+        data_dict['voxels'] = voxels #dim =(num_voxels, max_points_per_voxel=5, coords+feats = 3+1=4), gives xyzi value for each point in each voxel, if num points in voxel < 5, fill in zeros
+        data_dict['voxel_coords'] = coordinates #dim= (num voxels, 3), gives z,y,x grid coord of each voxel 
+        data_dict['voxel_num_points'] = num_points # dim = num_voxels, gives num points in each voxel
+        return data_dict
     
     def prepare_data(self, data_dict):
         
         PLOT= False
         cfg = self.cfg
 
-        # GT sampling
-        # if "GT_SAMPLING" in self.cfg:
-        #     data_dict = self.db_sampler(data_dict)
-
-        # remove gt_boxes not in class_names  #TODO: approx boxes
-        # gt_boxes_mask = np.array([n in self.class_names for n in data_dict['gt_names']], dtype=np.bool_)
-        # data_dict['gt_boxes'] = data_dict['gt_boxes'][gt_boxes_mask]
-        # data_dict['gt_names'] = data_dict['gt_names'][gt_boxes_mask]
-
         # remove points and boxes outside range
         data_dict['points'] = data_processor.mask_points_outside_range(data_dict['points'], self.point_cloud_range)
-        
-        #If we remove boxes outside range, then there could be some points in those boxes
-        # which are wihtin range and therefore have not been removed by mask_points_outside_range step.
-        # Hence, Don't mask boxes outside range, in the end we will filter boxes with few points.
-        # mask = data_processor.mask_boxes_outside_range(data_dict['gt_boxes'], self.point_cloud_range)
-        # data_dict['gt_boxes'] = data_dict['gt_boxes'][mask]
-
-        # for cluster_id in np.unique(data_dict['points'][:,-1]):
-        #     if cluster_id == -1:
-        #         continue
-        #     assert cluster_id in data_dict['gt_boxes'][:,-1]
-
-
-        # data_dict['gt_box_cluster_ids'] = data_dict['gt_box_cluster_ids'][mask]
-        # data_dict['gt_names'] = data_dict['gt_names'][mask]
+    
 
         if PLOT:
             # After gt sampling and cropping
             V.draw_scenes(points=data_dict["points"][:,:4], gt_boxes=data_dict["gt_boxes"][:,:7], color_feature='intensity')
-
-        # # TODO: not needed, remove
-        # if 'gt_boxes2d' in data_dict:
-        #     data_dict['gt_boxes2d'] = data_dict['gt_boxes2d'][gt_boxes_mask][mask]
 
         if self.pretraining:
             assert len(data_dict['points']) > 0
@@ -199,38 +162,13 @@ class DepthContrastDataset(Dataset):
             if self.pretraining:
                 data_dict['points_moco'] = data_processor.shuffle_points(data_dict['points_moco'])
 
-        # Add class_index to gt boxes and keep note of gtbox id TODO: is this needed?
-        # num_gt_boxes = data_dict['gt_boxes'].shape[0]
-        # gt_classes = np.ones(num_gt_boxes) #np.array([self.class_names.index(n) + 1 for n in data_dict['gt_names']], dtype=np.int32)
-        # #gt_box_ids = np.array([i for i in range(num_gt_boxes)], dtype=np.int32)
-
-        # assert data_dict['gt_boxes'].shape == data_dict['gt_boxes_moco'].shape
-        # #append class id as 8th entry in gt boxes
-        # data_dict['gt_boxes'] = np.concatenate((data_dict['gt_boxes'], gt_classes.reshape(-1, 1).astype(np.float32)), axis=1)
-        # #data_dict['gt_boxes_idx'] = gt_box_ids #.reshape(-1, 1).astype(np.float32) #TODO: remove if not needed
-
-        # #append class id as 8th entry in gt boxes
-        # data_dict['gt_boxes_moco'] = np.concatenate((data_dict['gt_boxes_moco'], gt_classes.reshape(-1, 1).astype(np.float32)), axis=1)
-        #data_dict['gt_boxes_moco_idx'] = gt_box_ids #.reshape(-1, 1).astype(np.float32)
-
         # for per point fg,bg prediction
         if self.mode == 'train':
             # If augmentor removes a patch with gt box, remove its gt box and label its points as -1
-            # mask, box_ids_of_pts = data_processor.mask_boxes_with_few_points(data_dict['points'], data_dict['gt_boxes'])
-            # data_dict['gt_boxes'] = data_dict['gt_boxes'][mask]
-            # data_dict['gt_boxes_idx'] = data_dict['gt_boxes_idx'][mask]
-            # data_dict['box_ids_of_pts'] = box_ids_of_pts 
             data_dict['points'], data_dict['gt_boxes'] = data_processor.mask_boxes_with_few_points(data_dict['points'], data_dict['gt_boxes'])
             if self.pretraining:
                 data_dict['points_moco'], data_dict['gt_boxes_moco'] = data_processor.mask_boxes_with_few_points(data_dict['points_moco'], data_dict['gt_boxes_moco'])
-                # mask, box_ids_of_pts_moco = data_processor.mask_boxes_with_few_points(data_dict['points_moco'], data_dict['gt_boxes_moco'])
-                # data_dict['gt_boxes_moco'] = data_dict['gt_boxes_moco'][mask]
-                # data_dict['gt_boxes_moco_idx'] = data_dict['gt_boxes_moco_idx'][mask]
-                # data_dict['box_ids_of_pts_moco'] = box_ids_of_pts_moco
-                # get unique labels from pcd_i and pcd_j
-            
-        # common_obj_idx = list(set(data_dict['gt_boxes_idx']) & set(data_dict['gt_boxes_idx']))
-        # assert len(common_obj_idx) > 0
+
         
         if PLOT:
             # After sampling points and removing empty boxes
@@ -244,34 +182,12 @@ class DepthContrastDataset(Dataset):
             if self.pretraining:
                 vox_dict = self.toVox(data_dict["points_moco"])
                 data_dict["vox_moco"] = vox_dict
-        # else:
-        #     #transform to tensor
-        #     data_dict['data'] = data_dict.pop('points')
-        #     if self.pretraining:
-        #         data_dict['data_moco'] = data_dict.pop('points_moco')
-
-        # #TODO: delete unnecessary 
-        # if 'calib' in data_dict:
-        #     data_dict.pop('calib')
-        # if 'road_plane' in data_dict:
-        #     data_dict.pop('road_plane')
 
         data_dict['gt_boxes_cluster_ids'] = data_dict['gt_boxes'][:,-1]
         data_dict['gt_boxes'] = data_dict['gt_boxes'][:, :8] #xyz,lwh, rz, gt class_index i.e. 1: Vehicle, ...
         if self.pretraining:
             data_dict['gt_boxes_moco_cluster_ids'] = data_dict['gt_boxes_moco'][:,-1]
             data_dict['gt_boxes_moco'] = data_dict['gt_boxes_moco'][:, :8]
-
-        # for cluster_id in np.unique(data_dict['points'][:,-1]):
-        #     if cluster_id == -1:
-        #         continue
-        #     assert cluster_id in data_dict['gt_boxes_cluster_ids']
-
-        # for cluster_id in np.unique(data_dict['points_moco'][:,-1]):
-        #     if cluster_id == -1:
-        #         continue
-        #     assert cluster_id in data_dict['gt_boxes_moco_cluster_ids']
-        return data_dict
 
     def __getitem__(self, idx):
         """
@@ -286,14 +202,3 @@ class DepthContrastDataset(Dataset):
 
         """
         raise NotImplementedError
-
-    # def get_batchsize_per_replica(self):
-    #     # this searches for batchsize_per_replica in self and then in self.dataset
-    #     return getattr(self, "batchsize_per_replica", 1)
-
-    # def get_global_batchsize(self):
-    #     if torch.distributed.is_available() and torch.distributed.is_initialized():
-    #         world_size = torch.distributed.get_world_size()
-    #     else:
-    #         world_size = 1
-    #     return self.get_batchsize_per_replica() * world_size
