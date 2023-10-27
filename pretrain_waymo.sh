d763742e10d6d3117372db783e2f189a8209f764
Barza Nisar <barzanisar93@gmail.com>
2023-10-27 02:28:37 -0400

    only try 1 node to find max batchsize

--- pretrain_waymo.sh
+++ pretrain_waymo.sh
@@ -1,5 +1,5 @@
-sbatch --time=00:30:00 --array=1-1%1 --job-name=pretrain_waymo_short_multinode scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19664 --cfg_file configs/waymo_pointrcnn.yaml
-sbatch --time=00:30:00 --array=1-1%1 --job-name=pretrain_waymo_short_multinode scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19667 --cfg_file configs/waymo_centerpoint.yaml
+sbatch --time=01:00:00 --array=1-1%1 --job-name=pretrain_waymo_short_multinode scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19664 --cfg_file configs/waymo_pointrcnn.yaml
+sbatch --time=01:00:00 --array=1-1%1 --job-name=pretrain_waymo_short_multinode scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19667 --cfg_file configs/waymo_centerpoint.yaml
 
 # needs 1 hr 14 min for each split (199 sequences) of waymo_10% -> should take around 12-13 hours for each 200 seq split of full waymo
 # check if --processed_data_tag waymo_processed_data_v_1_2_0 in /home/barza/DepthContrast/scripts/submit_compute_canada_cluster_waymo.sh
