7eb9a67a19adef6c80188662305c4b3bc5f29280
Barza Nisar <barzanisar93@gmail.com>
2023-11-30 21:30:17 -0500

    cluster sbatch train and val

--- cluster_waymo_sbatch.sh
+++ cluster_waymo_sbatch.sh
@@ -20,11 +20,11 @@ sbatch --time=05:00:00 --array=1-1%1 --job-name=cluster_waymo_13 scripts/submit_
 sbatch --time=05:00:00 --array=1-1%1 --job-name=cluster_waymo_14 scripts/submit_compute_canada_cluster_waymo.sh --split train_14
 sbatch --time=05:00:00 --array=1-1%1 --job-name=cluster_waymo_15 scripts/submit_compute_canada_cluster_waymo.sh --split train_15
 
-# sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_0 scripts/submit_compute_canada_cluster_waymo.sh --split val_0
-# sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_1 scripts/submit_compute_canada_cluster_waymo.sh --split val_1
-# sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_2 scripts/submit_compute_canada_cluster_waymo.sh --split val_2
-# sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_3 scripts/submit_compute_canada_cluster_waymo.sh --split val_3
-# sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_4 scripts/submit_compute_canada_cluster_waymo.sh --split val_4
-# sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_5 scripts/submit_compute_canada_cluster_waymo.sh --split val_5
-# sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_6 scripts/submit_compute_canada_cluster_waymo.sh --split val_6
-# sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_7 scripts/submit_compute_canada_cluster_waymo.sh --split val_7
+sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_0 scripts/submit_compute_canada_cluster_waymo.sh --split val_0
+sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_1 scripts/submit_compute_canada_cluster_waymo.sh --split val_1
+sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_2 scripts/submit_compute_canada_cluster_waymo.sh --split val_2
+sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_3 scripts/submit_compute_canada_cluster_waymo.sh --split val_3
+sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_4 scripts/submit_compute_canada_cluster_waymo.sh --split val_4
+sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_5 scripts/submit_compute_canada_cluster_waymo.sh --split val_5
+sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_6 scripts/submit_compute_canada_cluster_waymo.sh --split val_6
+sbatch --time=03:00:00 --array=1-1%1 --job-name=cluster_waymo_val_7 scripts/submit_compute_canada_cluster_waymo.sh --split val_7
