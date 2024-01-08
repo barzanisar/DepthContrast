4da1ac010c080721ce537037891de3f7578e2c2b
Barza Nisar <barzanisar93@gmail.com>
2024-01-08 11:00:08 -0500

    add torch-scatter install to docker

--- pretrain_waymo_exp.sh
+++ pretrain_waymo_exp.sh
@@ -112,6 +112,14 @@ sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-6%1 --job-name=esf_thresh_0
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-6%1 --job-name=esf_perc_0p2 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19214 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p2.yaml
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-6%1 --job-name=esf_perc_0p5 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19215 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p5.yaml
 
+sbatch --time=00:10:00 --nodes=1 --ntasks=1 --array=1-1%1 --job-name=esf_thresh_0p1 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19211 --cfg_file configs/waymo_pointrcnn_backbone_esf_thresh_0p1.yaml
+
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-3%1 --job-name=esf_thresh_0p1 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19211 --cfg_file configs/waymo_pointrcnn_backbone_esf_thresh_0p1.yaml
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-3%1 --job-name=esf_thresh_0p3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19212 --cfg_file configs/waymo_pointrcnn_backbone_esf_thresh_0p3.yaml
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-3%1 --job-name=esf_thresh_0p4 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19213 --cfg_file configs/waymo_pointrcnn_backbone_esf_thresh_0p4.yaml
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-3%1 --job-name=esf_perc_0p2 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19214 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p2.yaml
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-3%1 --job-name=esf_perc_0p5 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19215 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p5.yaml
+
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-6%1 --job-name=iou_thresh_0p3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19216 --cfg_file configs/waymo_pointrcnn_backbone_iou_thresh_0p3.yaml
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-6%1 --job-name=iou_thresh_0p4 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19217 --cfg_file configs/waymo_pointrcnn_backbone_iou_thresh_0p4.yaml
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-6%1 --job-name=iou_thresh_0p5 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19218 --cfg_file configs/waymo_pointrcnn_backbone_iou_thresh_0p5.yaml
