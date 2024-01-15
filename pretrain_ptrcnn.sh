c444ec1d5020eb8b6fda4a0d809127f13b0d94ec
Barza Nisar <barzanisar93@gmail.com>
2024-01-15 18:11:39 -0500

    esf iou combo ptrcnn

--- pretrain_ptrcnn.sh
+++ pretrain_ptrcnn.sh
@@ -4,12 +4,13 @@ sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19213 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p3.yaml
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p4 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19214 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p4.yaml
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p5 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19215 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p5.yaml
+
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p01 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19216 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p01.yaml
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=iou_perc_0p01 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19217 --cfg_file configs/waymo_pointrcnn_backbone_iou_perc_0p01.yaml
-sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p025 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19216 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p025.yaml
-sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=iou_perc_0p025 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19217 --cfg_file configs/waymo_pointrcnn_backbone_iou_perc_0p025.yaml
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p025 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19218 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p025.yaml
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=iou_perc_0p025 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19219 --cfg_file configs/waymo_pointrcnn_backbone_iou_perc_0p025.yaml
 
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=iou_perc_0p05_iou_wt_w3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19216 --cfg_file configs/waymo_pointrcnn_backbone_iou_perc_0p05_iou_wt.yaml
 sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=iou_perc_0p05_esf_wt_w3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19217 --cfg_file configs/waymo_pointrcnn_backbone_iou_perc_0p05_esf_wt.yaml
-# sbatch --time=5:00:00 --nodes=1 --ntasks=1 --array=1-6%1 --job-name=esf_perc_0p3_iou_wt_w3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19218 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p3_iou_wt.yaml
-# sbatch --time=5:00:00 --nodes=1 --ntasks=1 --array=1-6%1 --job-name=esf_perc_0p3_esf_wt_w3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19219 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p3_esf_wt.yaml
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p05_iou_wt_w3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19218 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p05_iou_wt.yaml
+sbatch --time=3:00:00 --nodes=1 --ntasks=1 --array=1-10%1 --job-name=esf_perc_0p05_esf_wt_w3 scripts/submit_ddp_compute_canada_waymo_multinode.sh --tcp_port 19219 --cfg_file configs/waymo_pointrcnn_backbone_esf_perc_0p05_esf_wt.yaml
