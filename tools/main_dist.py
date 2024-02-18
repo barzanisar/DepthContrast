b5d95b433c7f00063ce1bb33f45c5c0c7db3202b
Barza Nisar <barzanisar93@gmail.com>
2024-02-18 09:00:00 -0500

    scratch 5 percent turing

--- tools/main_dist.py
+++ tools/main_dist.py
@@ -11,7 +11,7 @@ import random
 import time
 import warnings
 import yaml
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+# os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 import torch
 import torch.nn.parallel
